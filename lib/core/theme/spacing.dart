6760efab11769252e830a2c03189e5a688565034
Ahmed Abdelnasser <ahmedabdelnasser.dev@gmail.com>
2025-10-16 20:50:45 +0300

    feat(core): Added more core structure to the project - Added the Colors, font styles, spacing to the project - Removed the main_button.dart as it does not need a custom widget
    Now we finished Setting up the project

--- lib/core/theme/spacing.dart
+++ lib/core/theme/spacing.dart
@@ -1,5 +1,11 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Spacing {
+  //? Main App Spacing
   static double appHorizontalMargin = 16.w;
+  static double appVerticalMargin = 10.h;
+
+  //? Common Spacing
+  static double verticalSpace = 20.h;
+  static double horizontalSpace = 10.w;
 }
